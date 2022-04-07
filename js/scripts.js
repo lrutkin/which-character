98eb2525d8cf679ce0f6fc1139971547513fc502
Leslie Rutkin <lrutkin@gmail.com>
2022-04-06 19:57:03 -0700

    add js quiz function
    Co-authored-by: Shelby Trodden <shelby.trodden@gmail.com

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,27 @@
 $(document).ready(function(){
-  $(".quiz")
-}
+  $("#character").submit(function(event){
+    event.preventDefault();
+
+      let qOneInput=parseInt($("#color").val());
+      let qTwoInput=parseInt($("#season").val());
+      let qThreeInput=parseInt($("#pet").val());
+
+      $("#color").text(qOneInput);
+      $("#season").text(qTwoInput);
+      $("#pet").text(qThreeInput);
+
+      let final=qOneInput+qTwoInput+qThreeInput
+        if (final <=3) {
+          $("#result1").show();
+        } else if (final <7){
+          $("#result2").show();
+        } else if (final <10){
+          $("#result3").show();
+        } else 
+          $("#result4").show();
+
+ 
+          
+
+  });
+});
